f8fd855d95ba8c5d59de9b8746505950ea51aa29
Risriddle <risriddle62442@gmail.com>
2025-05-07 09:52:59 +0530

    resolved canvas issue

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config, { isServer }) {
+    config.module.rules.push({
+      test: /\.node$/,
+      use: 'raw-loader',
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
